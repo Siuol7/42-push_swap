e02b3bf9ebf963d6286004c8b3dd15064852e88e
Siuol7 <nguyencaotan03072000@gmail.com>
2024-12-18 08:01:07 +0200

    Rename dir

--- execution/pre push_swap/sorting.c
+++ execution/pre push_swap/sorting.c
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   sorting.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/17 10:00:39 by caonguye          #+#    #+#             */
-/*   Updated: 2024/12/18 08:00:54 by caonguye         ###   ########.fr       */
+/*   Created: 2024/12/18 07:58:35 by caonguye          #+#    #+#             */
+/*   Updated: 2024/12/18 08:00:19 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include "./library/ft_printf_fd/ft_printf.h"
-# include "./library/libft/libft.h"
-
-typedef struct s_node
-{
-	struct s_node	*next;
-	struct s_node	*prev;
-	int				val;
-	int				id;
-}	t_node;
-
-
-
-#endif
+#include "./headers/push_swap.h"
