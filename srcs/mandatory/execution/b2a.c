bcb9680451481051410f125164f5ef0089321be4
Siuol7 <nguyencaotan03072000@gmail.com>
2025-01-08 11:24:47 +0200

    Update new b2a

--- srcs/mandatory/execution/b2a.c
+++ srcs/mandatory/execution/b2a.c
@@ -6,13 +6,13 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 15:56:44 by hitran            #+#    #+#             */
-/*   Updated: 2025/01/08 04:09:12 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/01/08 11:24:18 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	get_position(t_stack *stack, int highest, int size)
+static int	find_pos(t_stack *stack, int target, int size)
 {
 	int		position;
 	t_node	*node;
@@ -21,7 +21,7 @@ static int	get_position(t_stack *stack, int highest, int size)
 	position = 0;
 	while (position < size && node)
 	{
-		if (node->id == highest)
+		if (node->id == target)
 			return (position);
 		position++;
 		node = node->prev;
@@ -29,30 +29,30 @@ static int	get_position(t_stack *stack, int highest, int size)
 	return (-1);
 }
 
-static int	sort_a(t_pushswap *ps, int *ra_steps, int *rra_steps, int *highest)
+static int	sort_a(t_pushswap *ps, int *ra_steps, int *rra_steps, int *target)
 {
 	if (ps->stack_a->size == 0)
 		return (0);
-	if (ps->stack_a->bottom->id == *highest)
+	if (ps->stack_a->bottom->id == *target)
 	{
 		rra(ps);
 		(*rra_steps)--;
-		(*highest)--;
+		(*target)--;
 		return (1);
 	}
-	else if (get_position(ps->stack_a, *highest, *ra_steps) != -1)
+	else if (find_pos(ps->stack_a, *target, *ra_steps) != -1)
 	{
 		(*ra_steps)--;
-		(*highest)--;
+		(*target)--;
 		return (1);
 	}
 	return (0);
 }
 
-static int	push_highest(t_pushswap *ps, int *ra_steps,
-				int *rra_steps, int *highest)
+static int	push_target(t_pushswap *ps, int *ra_steps,
+				int *rra_steps, int *target)
 {
-	if (!ps->stack_b->size || *highest != ps->stack_b->top->id)
+	if (!ps->stack_b->size || *target != ps->stack_b->top->id)
 		return (0);
 	while (*ra_steps > 1)
 	{
@@ -63,11 +63,11 @@ static int	push_highest(t_pushswap *ps, int *ra_steps,
 	pa(ps);
 	if (*ra_steps == 1)
 		sa(ps);
-	(*highest)--;
+	(*target)--;
 	return (1);
 }
 
-static int	can_push_to_a(t_pushswap *ps, int *ra_steps, int *rra_steps)
+static int	pushing(t_pushswap *ps, int *ra_steps, int *rra_steps)
 {
 	if (!ps->stack_b->size)
 		return (0);
@@ -90,22 +90,22 @@ void	b2a(t_pushswap *ps)
 {
 	int	ra_steps;
 	int	rra_steps;
-	int	highest;
+	int	target;
 
 	ra_steps = 0;
 	rra_steps = 0;
-	highest = ps->stack_b->size - 1;
+	target = ps->stack_b->size - 1;
 	while (ps->stack_b->size || ra_steps > 0 || rra_steps > 0)
 	{
-		if (sort_a(ps, &ra_steps, &rra_steps, &highest))
+		if (sort_a(ps, &ra_steps, &rra_steps, &target))
 			continue ;
-		else if (push_highest(ps, &ra_steps, &rra_steps, &highest))
+		else if (push_target(ps, &ra_steps, &rra_steps, &target))
 			continue ;
-		else if (can_push_to_a(ps, &ra_steps, &rra_steps))
+		else if (pushing(ps, &ra_steps, &rra_steps))
 			continue ;
-		else if (get_position(ps->stack_b, highest, ps->stack_b->size) > (ps->stack_b->size / 2))
+		else if (find_pos(ps->stack_b, target, ps->stack_b->size) > (ps->stack_b->size / 2))
 			rrb(ps);
-		else if (get_position(ps->stack_b, highest, ps->stack_b->size) > -1)
+		else if (find_pos(ps->stack_b, target, ps->stack_b->size) > -1)
 			rb(ps);
 	}
 }
