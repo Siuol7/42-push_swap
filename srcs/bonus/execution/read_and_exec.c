478343f807c79736803f225577daeff6fdd2a9f2
Siuol7 <nguyencaotan03072000@gmail.com>
2025-01-10 02:44:59 +0200

    Update checker

--- srcs/bonus/execution/read_and_exec.c
+++ srcs/bonus/execution/read_and_exec.c
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 06:54:35 by caonguye          #+#    #+#             */
-/*   Updated: 2025/01/09 09:55:42 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/01/10 02:38:08 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,16 @@ static char *get_ops(t_pushswap *ps, char *line)
 		return (NULL);
 	ft_memcpy(ops, line, length - 1);
 	ops[length - 1] = '\0';
+	if (ft_strcmp(ops, "sa") != 0 || ft_strcmp(ops, "sb") != 0
+		|| ft_strcmp(ops, "ss") != 0 || ft_strcmp(ops, "ra") != 0
+		|| ft_strcmp(ops, "rb") != 0 || ft_strcmp(ops, "rr") != 0
+		|| ft_strcmp(ops, "rra") != 0 || ft_strcmp(ops, "rrb") != 0
+		|| ft_strcmp(ops, "rrr") != 0 || ft_strcmp(ops, "pa") != 0
+		|| ft_strcmp(ops, "pba") != 0)
+	{
+		free(ops);
+		return (NULL);
+	}
 	parse_ops(ps, ops);
 	free(ops);
 	return (ops);
@@ -69,13 +79,14 @@ int	read_and_exec(t_pushswap *ps)
 			ft_printf_fd(2, "Error\n");
 			return (0);
 		}
-		else if (readbytes == 0 && line[0] == '\0')
-			return (1);
 		line[readbytes] = '\0';
-		if ((line[readbytes - 1] != '\n') || !get_ops(ps, line))
+		if ((line[readbytes - 1] != '\n') || !get_ops(ps, line)
+			|| line[0] == '\n')
 		{
 			ft_printf_fd(2, "Error\n");
 			return (0);
 		}
+		if (readbytes == 0 && line[0] == '\0')
+			return (1);
 	}
 }
